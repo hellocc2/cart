14c05e43033317f04da06e00dc4de0f518398304
hellocc2 <jiang163cai@gmail.com>
2017-12-05 08:31:52 +0000

    :q! :exit :exit! :Q!

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.chinapepper.com/ocart/admin/');
+define('HTTP_CATALOG', 'http://www.chinapepper.com/ocart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.chinapepper.com/ocart/admin/');
+define('HTTPS_CATALOG', 'http://www.chinapepper.com/ocart/');
+
+// DIR
+define('DIR_APPLICATION', '/home/chinab3i/public_html/ocart/admin/');
+define('DIR_SYSTEM', '/home/chinab3i/public_html/ocart/system/');
+define('DIR_DATABASE', '/home/chinab3i/public_html/ocart/system/database/');
+define('DIR_LANGUAGE', '/home/chinab3i/public_html/ocart/admin/language/');
+define('DIR_TEMPLATE', '/home/chinab3i/public_html/ocart/admin/view/template/');
+define('DIR_CONFIG', '/home/chinab3i/public_html/ocart/system/config/');
+define('DIR_IMAGE', '/home/chinab3i/public_html/ocart/image/');
+define('DIR_CACHE', '/home/chinab3i/public_html/ocart/system/cache/');
+define('DIR_DOWNLOAD', '/home/chinab3i/public_html/ocart/download/');
+define('DIR_LOGS', '/home/chinab3i/public_html/ocart/system/logs/');
+define('DIR_CATALOG', '/home/chinab3i/public_html/ocart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'chinab3i_ocar998');
+define('DB_PASSWORD', 'op79[yS74@');
+define('DB_DATABASE', 'chinab3i_ocar998');
+define('DB_PREFIX', 'ockv_');
+?>
